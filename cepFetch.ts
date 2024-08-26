c151b15a322ea518e4cf7b9d538beb58a3f120da
Robson Ferreira de Fairas <robsonfdfarias@gmail.com>
2024-08-26 13:05:52 -0300

    adicionado o filtro para o formato do CEP

--- cepFetch.ts
+++ cepFetch.ts
@@ -1,16 +1,21 @@
 class CepFetch{
     cep: string;
     url: string;
+    data=null;
     constructor(){
         this.cep = "89260854";
         this.url = 'https://viacep.com.br/ws/';
     }
+    
     async fetch(cep: string, car: HTMLElement){
-        if(cep != undefined){
+        const cepPattern = /^\d{5}-\d{3}$/;
+        if(cepPattern.test(cep)){
             this.cep = cep;
+        }else{
+            alert("Formato de CEP inválido. O CEP deve estar no formato: 99999-999");
+            return;
         }
         this.url = this.url+this.cep+'/json/';
-        var data=null;
         car.style.display = 'block';
         await fetch(this.url, {
             method: 'GET'
@@ -18,12 +23,12 @@ class CepFetch{
         .then((response)=>response.json())
         .then((json)=>{
             // console.log(json)
-            data = json;
+            this.data = json;
         })
         .catch((error)=>{
             console.log('Erro encontrado: -> '+error);
         })
         car.style.display = 'none';
-        return data;
+        return this.data;
     }
 }
